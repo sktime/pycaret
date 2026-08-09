7cd238826db73310cc155cb0f6b751932b408ca7
Franz Király <fkiraly@gcos.ai>
2026-08-09 15:02:23 +0200

    Update __init__.py

--- pycaret/__init__.py
+++ pycaret/__init__.py
@@ -10,4 +10,4 @@ __all__ = ["show_versions", "__version__"]
 
 msg = "Pycaret only supports python 3.10 to 3.14."
 
-_check_python_version(">=3.10,<3.14", msg=msg)
+_check_python_version(">=3.10,<3.15", msg=msg)
