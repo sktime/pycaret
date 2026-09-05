7e99e578adfa20bfc56329bd3dd34dc9c40894c3
Andreas Motl <andreas.motl@panodata.org>
2026-09-05 13:26:41 +0200

    [MNT] Adjustments to `_check_soft_dependencies` invocations

--- pycaret/internal/pycaret_experiment/tabular_experiment.py
+++ pycaret/internal/pycaret_experiment/tabular_experiment.py
@@ -604,7 +604,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def cluster():
 
             # Import required libraries ----
-            _check_soft_dependencies("plotly", extra=None, severity="error")
+            _check_soft_dependencies("plotly", extra="plotting", severity="error")
             import plotly.express as px
 
             self.logger.info(
@@ -701,7 +701,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def umap():
 
             # Import required libraries ----
-            _check_soft_dependencies("plotly", extra=None, severity="error")
+            _check_soft_dependencies("plotly", extra="plotting", severity="error")
             import plotly.express as px
 
             _check_soft_dependencies(
@@ -783,7 +783,7 @@ class _TabularExperiment(_PyCaretExperiment):
             # Import required libraries ----
             from sklearn.manifold import TSNE
 
-            _check_soft_dependencies("plotly", extra=None, severity="error")
+            _check_soft_dependencies("plotly", extra="plotting", severity="error")
             import plotly.express as px
 
             self.logger.info(
@@ -867,7 +867,7 @@ class _TabularExperiment(_PyCaretExperiment):
             # Import required libraries ----
             from sklearn.manifold import TSNE
 
-            _check_soft_dependencies("plotly", extra=None, severity="error")
+            _check_soft_dependencies("plotly", extra="plotting", severity="error")
             import plotly.express as px
 
             self.logger.info(
@@ -968,7 +968,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def distribution():
 
             # Import required libraries ----
-            _check_soft_dependencies("plotly", extra=None, severity="error")
+            _check_soft_dependencies("plotly", extra="plotting", severity="error")
             import plotly.express as px
 
             self.logger.info(
@@ -1250,7 +1250,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def lift():
 
             # Import required libraries ----
-            _check_soft_dependencies("mljar-scikit-plot")
+            _check_soft_dependencies("mljar-scikit-plot", extra="plotting")
             import scikitplot as skplt
 
             self.logger.info("Generating predictions / predict_proba on X_test")
@@ -1279,7 +1279,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def gain():
 
             # Import required libraries ----
-            _check_soft_dependencies("mljar-scikit-plot")
+            _check_soft_dependencies("mljar-scikit-plot", extra="plotting")
             import scikitplot as skplt
 
             self.logger.info("Generating predictions / predict_proba on X_test")
@@ -1752,7 +1752,7 @@ class _TabularExperiment(_PyCaretExperiment):
         def ks():
 
             # Import required libraries ----
-            _check_soft_dependencies("mljar-scikit-plot")
+            _check_soft_dependencies("mljar-scikit-plot", extra="plotting")
             import scikitplot as skplt
 
             self.logger.info("Generating predictions / predict_proba on X_test")
@@ -2357,7 +2357,7 @@ class _TabularExperiment(_PyCaretExperiment):
 
         """
 
-        _check_soft_dependencies("m2cgen", extra=None, severity="error")
+        _check_soft_dependencies("m2cgen", extra="mlops", severity="error")
         import m2cgen as m2c
 
         if language == "python":
